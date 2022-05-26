38bd18c2717168b53157b022a9dc5d85d4f1895e
Avaneesh <avaneeshasokan@gmail.com>
2022-05-26 23:58:05 +0530

    - adding some resources - implementing splash screen

---
+++ app/src/main/java/com/edstem/mockup/activities/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.edstem.mockup.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.edstem.mockup.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/com/edstem/mockup/activities/SplashScreen.kt
@@ -0,0 +1,29 @@
+package com.edstem.mockup.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.edstem.mockup.databinding.ActivitySplashBinding
+
+class SplashScreen: AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(
+                Intent(
+                    this@SplashScreen,
+                    MainActivity::class.java
+                )
+            )
+            finish()
+        }, 3000)
+    }
+}
