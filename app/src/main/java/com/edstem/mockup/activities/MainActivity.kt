3587df0932a7a2fcbd08217a0934b63866564b01
Avaneesh <avaneeshasokan@gmail.com>
2022-05-28 11:07:33 +0530

    - setting up my profile UI  - user info, socials and contact info done

--- app/src/main/java/com/edstem/mockup/activities/MainActivity.kt
+++ app/src/main/java/com/edstem/mockup/activities/MainActivity.kt
@@ -2,16 +2,44 @@ package com.edstem.mockup.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.edstem.mockup.R
 import com.edstem.mockup.databinding.ActivityMainBinding
+import com.edstem.mockup.fragments.HomeFragment
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var homeFragment: HomeFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        bindListeners()
+    }
+
+    private fun bindListeners() {
+        with(binding){
+            bottomNav.setOnItemReselectedListener { item ->
+                when(item.itemId){
+                    R.id.home_menu -> {
+                        if (!this@MainActivity::homeFragment.isInitialized) {
+                            homeFragment = HomeFragment()
+                        }
+                        loadFragment(homeFragment)
+                    }
+                }
+            }
+        }
+    }
+
+    fun loadFragment(fragment: Fragment){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(binding.fragmentContainer.id, fragment)
+            .commit()
     }
 }
